72da5b2db0aaf145319a57f63a54a3ad8f095c1a
Bohdan Kolomiiets <bohdan_kolomiiets@epam.com>
2019-08-14 17:28:12 +0300

    added settings layout

--- app/src/main/java/com/example/practicetwo/MainActivity.java
+++ app/src/main/java/com/example/practicetwo/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewTaskActivity.class);
         startActivity(intent);
     }
+
+    public void settings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
 }
